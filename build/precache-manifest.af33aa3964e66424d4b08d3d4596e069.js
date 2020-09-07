cf110fe78230d98d921f973af2b54baf74f53ace
vishutiwari99 <vishutiwari99@gmail.com>
2020-09-07 23:21:45 +0530

    Error solved

---
+++ build/precache-manifest.af33aa3964e66424d4b08d3d4596e069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694cebd94ccddd91c382c352911ae06d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8677c5f095ff149c747b",
+    "url": "/static/css/main.b6e9c227.chunk.css"
+  },
+  {
+    "revision": "d138756cbd340d83454f",
+    "url": "/static/js/2.531f2f4b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.531f2f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8677c5f095ff149c747b",
+    "url": "/static/js/main.fb6dce9c.chunk.js"
+  },
+  {
+    "revision": "a9bcb2082ea44ffc1df4",
+    "url": "/static/js/runtime-main.ec8f8518.js"
+  }
+]);
